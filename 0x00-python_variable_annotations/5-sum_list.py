de6eb287bbb8aaa165dae218e468dcde9595b75f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-30 13:37:24 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""Display annotated function sum_list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns their sum as a float"""
+    return sum(input_list)
