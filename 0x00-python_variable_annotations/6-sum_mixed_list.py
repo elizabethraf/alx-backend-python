d00a1b96eadc04989fc1257cea89fda681fc5c0d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-30 14:09:43 +0000

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List, Union
+"""display -annotated function sum_mixed_list"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of the integers"""
+    return sum(mxd_lst)
