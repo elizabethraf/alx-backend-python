96089d4f4eaaef6131afbdcfc5e92de13f95fb06
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-03 12:37:09 +0000

    3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,8 +2,9 @@
 """Display  the regular function"""
 import asyncio
 import random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay):
     """Return a asyncia.taks"""
-    return asyncio.create_task(wait_random(max_delay=max_delay))
+    return asyncio.create_task(wait_random(max_delay))
