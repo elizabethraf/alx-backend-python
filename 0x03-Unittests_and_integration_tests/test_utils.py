cdb231fc916317136e290a168333bdd06dcdecf6
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-24 14:04:51 +0000

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Display utils.access_nested_map function""" 
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_result)
